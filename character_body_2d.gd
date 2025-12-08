54d0ef82549bd8e959ea630f9a7d8b7f11b8e4db
Snakatakka <etwork563@gmail.com>
2025-12-08 17:36:54 -0500

    added player movement, as well as started work on bullet (putt?) time and scene transitions

---
+++ character_body_2d.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+
+
+const SPEED = 1000.0
+const JUMP_VELOCITY = -800.0
+var was_on_floor = is_on_floor
+var near_golfball = false
+
+@onready var coyote: Timer = $Coyote
+@onready var landing_lag: Timer = $LandingLag
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and (is_on_floor() || !coyote.is_stopped()):
+		velocity.y = JUMP_VELOCITY
+	
+	
+	# Coyote Time
+	if was_on_floor && is_on_floor():
+		coyote.start()
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+# Player enters golfball's radius
+func _on_golfball_area_entered(area: Area2D) -> void:
+	near_golfball = true
+
+# Bullet Time
+func _on_golfball_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if near_golfball and Input.is_action_just_pressed("putt"):
+		if Engine.time_scale <= 0.1:
+			Engine.time_scale = 1.0
+		else:
+			Engine.time_scale = 0.1
